2ae037e3d1191022567b6ceac8a51e07d9db88ac
mrakestraw777 <57491480+mrakestraw777@users.noreply.github.com>
2021-09-21 14:16:41 -0400

    Update cij.js

--- cij.js
+++ cij.js
@@ -1,6 +1,8 @@
 const mySelects = document.querySelectorAll("select");
 let searchDisplay = document.getElementById('searchResults');
 
+console.log(`Here's proof of my new commits working with the same url.`)
+
 mySelects.forEach(e =>{
   e.addEventListener('change', selectsChanged)
 })
